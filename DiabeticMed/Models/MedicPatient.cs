5195a93207f96a39ea45864c6259a3fed06a9f44
edd <edwinkamaumuraya0@outlook.com>
2018-11-10 23:36:00 +0300

    added model for the patients required details

---
+++ DiabeticMed/Models/MedicPatient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiabeticMed.Models
+{
+    public class MedicPatient
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Pregnanicies { get; set; }
+        [Required]
+        public float PlasmaGlucose { get; set; }
+        [Required]
+        public float DiasotolicBloodPressure  { get; set; }
+        [Required]
+        public float TricepThickness { get; set; }
+        [Required]
+        public float Seruminium { get; set; }
+        [Required]
+        public float BMi { get; set; }
+        [Required]
+        public float DiabeticPadiac  { get; set; }
+        [Required]
+        public float Age { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public float Diabetic { get; set; }
+
+    }
+}
