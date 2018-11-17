3a5f6b102ad414b757c90c06c95dce456ee4d92c
edd <edwinkamaumuraya0@outlook.com>
2018-11-17 13:24:30 +0300

    styling the web app

--- DiabeticMed/wwwroot/js/site.js
+++ DiabeticMed/wwwroot/js/site.js
@@ -3,3 +3,18 @@ $(window).on('load',function(){//make sure that the page is full loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut();
 });
+
+//owl-calosel
+$(function(){
+    $('#team-members').owlCarousel({
+        items:2,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+        nav:true,
+        dots:false,
+        navText:['<i class="fa fa-angle-left"></i>',
+                 '<i class="fa fa-angle-right"></i>']
+    });
+});
